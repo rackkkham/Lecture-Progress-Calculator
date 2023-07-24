adda39ac51bbf8b0824b27d43f6f39ffcf13eb7c
Rackham <garikamirkhanyan@yahoo.com>
2023-07-24 23:46:52 +0400

    Handle empty input, format text for new lines

--- script.js
+++ script.js
@@ -4,37 +4,45 @@ const currentLecture = document.querySelector(".current-lecture");
 headingText.textContent = "Calculate your lectures progression ⏳";
 
 document.querySelector(".button").addEventListener("click", function () {
-  let totalLecturesz = Number(document.querySelector(".total-lectures").value);
-  let currentLecturez = Number(
+  let totalLecturesInput = Number(
+    document.querySelector(".total-lectures").value
+  );
+  let currentLectureInput = Number(
     document.querySelector(".current-lecture").value
   );
 
   function percentageOfLectures(totalLectures, lectureNumber) {
     if (!lectureNumber && !totalLectures) {
-      return "No number!";
+      return "No number! ⛔️";
+    }
+    if (!totalLectures || isNaN(totalLectures)) {
+      return "Please enter the Number of total lectures.";
+    }
+    if (!lectureNumber || isNaN(lectureNumber)) {
+      return "Please enter your Current lecture number.";
     }
     if (
       lectureNumber > totalLectures ||
       lectureNumber <= 0 ||
       isNaN(lectureNumber)
     ) {
-      return `Invalid lecture number! Your current lecture number cannot be higher than the number of total lectures you.`;
+      return `Invalid lecture number! ⛔️<br>Your current lecture number cannot be higher than the number of total lectures you.`;
     }
     const text = `You have reached to the ${lectureNumber}th lecture out of ${totalLectures}, and you have completed ${(
       (lectureNumber / totalLectures) *
       100
-    ).toFixed(2)}% of the total course. You've got ${
+    ).toFixed(2)}% of the total course. <br> You've got ${
       totalLectures - lectureNumber
     } more lectures to go.`;
 
     if (lectureNumber === totalLectures) {
-      return "CONGRATULATIONS! YOU HAVE COMPLETED THE JAVASCRIPT COURSE!\nYOU HAVE MADE IT!";
+      return "CONGRATULATIONS! 🎉 <br>YOU HAVE COMPLETED THE JAVASCRIPT COURSE! <br>YOU HAVE MADE IT!";
     }
     return text;
   }
 
-  let completed = percentageOfLectures(totalLecturesz, currentLecturez);
+  let completed = percentageOfLectures(totalLecturesInput, currentLectureInput);
   console.log(completed);
 
-  document.querySelector("#output-text").textContent = completed;
+  document.querySelector("#output-text").innerHTML = completed;
 });
